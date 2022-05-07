85072b2ba8ca015aff450026cc4f9e6f2711b8b9
abedalkareem omreyh <abedalkareem.omreyh@yahoo.com>
2022-05-07 20:43:07 +0400

    add profiling

---
+++ CCode/profiling/profiling.c
@@ -0,0 +1,18 @@
+//
+//  profiling.c
+//  CCode
+//
+//  Created by abedalkareem omreyh on 07/05/2022.
+//
+
+#include "profiling.h"
+
+// profiling is a form of dynamic program analysis that measures:
+// - space (memory)
+// - time complexity off a program (efficiency)
+// - usage of particular instrctions
+// - the frequency and duration of function calls
+
+// tools for profiling: gprof, valgrind.
+// to check for memory leaks:
+// $ valgrind --leak-check=yes ./test
---
+++ CCode/profiling/profiling.h
@@ -0,0 +1,13 @@
+//
+//  profiling.h
+//  CCode
+//
+//  Created by abedalkareem omreyh on 07/05/2022.
+//
+
+#ifndef profiling_h
+#define profiling_h
+
+#include <stdio.h>
+
+#endif /* profiling_h */
